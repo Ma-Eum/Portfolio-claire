6774eae1c193b294b07470f9246ddf0e33d16df9
Ma-Eum <cl.gachelin@gmail.com>
2026-04-03 14:59:53 +0200

    fix: simplification de la transition vers une page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,42 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import ScrollToTop from './components/ScrollToTop/ScrollToTop'
 
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
 import Project from './pages/Project/Project'
 import NotFound from './pages/NotFound/NotFound'
 
+import './components/PageTransition/PageTransition.scss'
+
+function AnimatedRoutes() {
+  const location = useLocation()
+  const isProjectPage = location.pathname.startsWith('/project/')
+
+  return (
+    <div
+      key={location.pathname}
+      className={`page-transition ${
+        isProjectPage ? 'page-transition--project' : 'page-transition--default'
+      }`}
+    >
+      <Routes location={location}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/project/:id" element={<Project />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  )
+}
+
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
+
       <div className="app-shell">
         <a className="skip-link" href="#main-content">
           Aller au contenu principal
@@ -19,12 +45,7 @@ function App() {
         <Header />
 
         <main id="main-content" className="app-main">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/project/:id" element={<Project />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+          <AnimatedRoutes />
         </main>
 
         <Footer />
