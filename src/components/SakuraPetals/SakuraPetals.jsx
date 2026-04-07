52815f34f15ff8f560be8074d85d45012295ce78
Ma-Eum <cl.gachelin@gmail.com>
2026-04-07 23:47:48 +0200

    Creation du composant sakura pour l'unifier sur toutes les pages

---
+++ src/components/SakuraPetals/SakuraPetals.jsx
@@ -0,0 +1,18 @@
+import './SakuraPetals.scss'
+
+function SakuraPetals() {
+  return (
+    <div className="sakura-petals" aria-hidden="true">
+      <span className="petal petal--1" />
+      <span className="petal petal--2" />
+      <span className="petal petal--3" />
+      <span className="petal petal--4" />
+      <span className="petal petal--5" />
+      <span className="petal petal--6" />
+      <span className="petal petal--7" />
+      <span className="petal petal--8" />
+    </div>
+  )
+}
+
+export default SakuraPetals
